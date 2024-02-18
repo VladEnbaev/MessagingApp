fb98d15b44bb7e2a8b79e18833d6d1d1ed5a2b18
VladGradskiy <devvlad.gradskiy@gmail.com>
2024-02-18 16:20:16 +0300

    Initial Commit

---
+++ MessagingApp/MessagingAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  MessagingAppApp.swift
+//  MessagingApp
+//
+//  Created by Vlad Gradskiy on 18.02.2024.
+//  
+
+
+import SwiftUI
+
+@main
+struct MessagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
